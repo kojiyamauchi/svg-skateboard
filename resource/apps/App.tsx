555d52dd7d464d997a2ddd4f7ee29041d510fcbe
kojiyamauchi <all.the.small.things@me.com>
2020-08-05 02:00:25 +0900

    change of writing method.

--- resource/apps/App.tsx
+++ resource/apps/App.tsx
@@ -3,36 +3,40 @@
 */
 
 import * as React from 'react'
-import SkateBoard from '@/components/SkateBoard/View'
-import Counter from '@/components/Counter/View'
-import Timer from '@/components/Timer/View'
-import Wall from '@/components/Wall/View'
+import { SkateBoard } from '@/components/SkateBoard/View'
+import { Counter } from '@/components/Counter/View'
+import { Timer } from '@/components/Timer/View'
+import { Wall } from '@/components/Wall/View'
 import styled from 'styled-components'
 import backGround from '@/materials/images/background.png'
 import useCountSkateboard from '@/hooks/Count'
 import useTimer from '@/hooks/Timer'
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  position fixed;
-  background-image: url(${backGround});
-  background-repeat repeat
-  background-position top left
-  background-size 100px
-`
+type Props = {
+  className?: string
+}
 
-const App: React.FC = (): JSX.Element => {
+const AppComponent: React.FC<Props> = (props): JSX.Element => {
   const { count, onBomb } = useCountSkateboard()
   const { second, over } = useTimer()
   return (
-    <Wrapper>
+    <div className={props.className}>
       <SkateBoard fn={onBomb} stateOver={over} />
       <Counter stateCount={count} stateOver={over} />
       <Timer stateSecond={second} stateOver={over} />
       <Wall stateCount={count} stateOver={over} />
-    </Wrapper>
+    </div>
   )
 }
 
-export default App
+const StyledComponent = styled(AppComponent)`
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  background-image: url(${backGround});
+  background-repeat: repeat;
+  background-position: top left;
+  background-size: 100px;
+`
+
+export const App = StyledComponent
